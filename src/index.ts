f86be8a52e462712648560315915f4e8aa0e988a
AlanNuevo <Andre.Sanchez@igrtec.com>
2024-03-08 18:41:20 -0600

    hola

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-import express from 'express'
-import AnimalsRouter from './animals/infraestructure/AnimalRoutes'
-import ZooRouter from './zoo/infraestructure/ZooRoutes'
+import express from 'express';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import AnimalsRouter from './animals/infraestructure/AnimalRoutes';
+import ZooRouter from './zoo/infraestructure/ZooRoutes';
+
 const app = express();
+app.use(cors());
 app.disable("x-powered-by");
 
+const accountLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hora
+    max: 1, // Límite de 6 solicitudes por hora
+    message: "Demasiadas peticiones realizadas, intenta después de 1 hora"
+});
 
+const port = process.env.PORT || 4000;
+app.use(express.json());
 
-const port = process.env.PORT || 4000
-app.use(express.json())
-
-app.use('/api/animals', AnimalsRouter)
-app.use('/api/Zoo', ZooRouter)
+app.use('/api/animals', accountLimiter, AnimalsRouter);
+app.use('/api/zoo', ZooRouter);
 
 app.listen(port, () => {
-    console.log('Server Servido en la puerta ' + port)
-})
+    console.log(`Server servido en la puerta ${port}`);
+});
